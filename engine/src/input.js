503d3b60f19f040bd6fa7c19996f4a69516d3b11
alexan32 <sethalexander32@gmail.com>
2021-05-15 07:54:45 -0400

    some stuff

--- engine/src/input.js
+++ engine/src/input.js
@@ -1,21 +1,49 @@
+import { EventEmitter } from "./utils.js";
+
 export class Input{
 
+    /*  Provides solution for common input needs. To customize behavior, 
+        replace the default behavior, overwrite the setEventBehavior() function.
+
+        Default Events:
+        - noKeyInput: fires when there is no keyboard buttons being pressed.
+        - noDirectionalInput: fires when there are no wasd or arrow keys pressed.
+    */
+
     constructor(canvas){
-        this.gameCanvas = canvas
+        this.gameCanvas = canvas;
         this.pressed = {};
+        this.directional = {};
         this.mouse = {
             x: 0,
             y: 0,
             down: false
         };
+        this.events = new EventEmitter();
+        this.setEventBehavior();
+    }
+
+    // overwrite to set create custom input behavior.
+    setEventBehavior(){
         window.addEventListener("keydown", event =>{
             this.pressed[event.code] = true;
-            // console.log(pressed);
+            if(event.code in ["KeyW", "KeyA", "KeyS", "KeyD", "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"]){
+                this.directional[event.code] = true;
+            }
         });
         window.addEventListener("keyup", event =>{
             delete this.pressed[event.code];
-            // console.log(pressed);
+            if(Object.keys(this.pressed).length === 0){
+                this.events.trigger("noKeyInput");
+            }
+            if(this.directional.hasOwnProperty(event.code)){
+                delete this.directional[event.code];
+                if(Object.keys(this.directional).length === 0){
+                    this.events.trigger("noDirectionalInput");
+                }
+            }
         });
+
         this.gameCanvas.addEventListener('mousemove', event => this.onMousemove(event));
         window.addEventListener('mouseup', event => this.onMouseup(event));
         window.addEventListener('mousedown', event => this.onMousedown(event));
--- project/tile/obj/entity.js
+++ project/tile/obj/entity.js
@@ -1,17 +1,89 @@
 import { GameObject } from "../../../engine/src/gameObject.js";
-import { Coord } from "../../../engine/src/utils.js";
+import { Coord, State, StateMachine } from "../../../engine/src/utils.js";
 import { SpriteAnimation, TileSet } from "./canvasTile.js";
 
 
-class Creature extends GameObject{
+/*  A Creature is a gameobject that can move in the world space and 
+    has animations for moving in each direction. The first frame of
+    each animation should be the still/idle frame.
+*/
 
-    constructor(x, y, tileset){
+class Creature extends GameObject {
+
+    constructor(x, y, tileset) {
         super();
         this.position = new Coord(x, y);
         this.tileset = tileset;
+        this.pauseAnimation = true;
+        this.directions = {
+            "S": 0,
+            "N": 1,
+            "W": 2,
+            "E": 3
+        }
+        this.facing = "S";
+        this.animations = buildCreatureAnimations(tileset);
     }
 
 }
+
+class AnimationController{
+
+    constructor(){
+        this.paused = false;
+    }
+
+    playOnce()
+
+}
+
+function buildAnimationController(animations){
+
+    var controller = {
+        animations: animations,
+        currentAnimation: 0,
+        stateMap:{
+            STANDING: 0,
+            WALKING: 0,
+            ACTION1: 4
+        },
+        directions :{
+            "S": 0,
+            "N": 1,
+            "W": 2,
+            "E": 3
+        },
+        facing: "S",
+        paused: true,
+        setCurrentAnimation: () => {
+            this.currentAnimation = this.stateMap[this.stateMachine.currentState] + this.directions[this.facing];
+        },
+        stateMachine: {}
+    }
+
+    var standing = new State({
+        moveStart: "WALKING",
+        performAction1: "ACTION1"
+    });
+    standing.onEnter = () =>{
+        controller.paused = true;
+    };
+
+    var walking = new State({
+        moveEnd: "STANDING"
+    });
+    walking.onEnter = () => {
+        controller.paused = false;
+    }
+
+    var action1 = new State({
+        animationEnd: "STANDING"
+    });
+    action1.onEnter = () => {
+        controller.paused = false;
+    }
+}
+
 
 /*  The first four rows of a Creatures tileset are reservered
     for movement in the four cardinal directions. The first frame
@@ -21,26 +93,14 @@ class Creature extends GameObject{
     animations. For example, rows 5,6,7,8 could be an attack animation
     and 9,10,11,12 could be an interaction animation.
 */
-function buildCreatureAnimations(tileset){
+function buildCreatureAnimations(tileset) {
     var animations = [];
-    for(var r=0; r<tileset.numRows; r++){
+    for (var r = 0; r < tileset.numRows; r++) {
         var frames = [];
-        for(var c=0; c<tileset.numColumns; c++){
+        for (var c = 0; c < tileset.numColumns; c++) {
             frames.push(tileset.getTile(c))
         }
         animations.push(new SpriteAnimation(frames));
     }
     return animations;
-}
-
-const machine = {
-
-    state: "WALK",
-    transitions: {
-        WALK:{
-            
-        }
-    }
-
-
 }
