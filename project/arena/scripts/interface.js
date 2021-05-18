890d8a2e11a0d9480569d3c199d923b9ba9e26b4
alexan32 <sethalexnader32@gmail.com>
2021-05-17 21:52:37 -0400

    interface stuff

--- project/arena/scripts/interface.js
+++ project/arena/scripts/interface.js
@@ -1,39 +1,63 @@
-import { GameObject } from "../../../engine/src/gameObject";
-import { Coord } from "../../../engine/src/utils";
+import { EventEmitter } from "../../../engine/src/utils.js";
+import { canvas } from "../../../engine/main.js";
 
 
 export class InterfaceTile{
 
-    constructor(x, y, w, h){
+    constructor(x, y, w, h, enableEvents=false){
         this.xOffset = x;
         this.yOffset = y;
+        this.screenX = 0;
+        this.screenY = 0;
         this.w = w;
         this.h = h;
+
         this.image = image;
         this.parent = null;
         this.children = [];
+
+        this.events = null;
+        if(enableEvents){
+            this.initializeEvents();
+        }
     }
 
-    /*  tile position is stored relative to parent.
-        getPosition works up tile tree to root, which has
-        an on screen position, and calculates the on screen 
-        position of the tile using that. 
+
+    /*  x,y is the calculated screen position of parent, so that 
+        child can calculate their own screen position 
     */
-    getPosition(){
-
-    }
-
-    draw(ctx){
-        ctx.drawImage(this.image, this.position.x, this.position.y);
-    }
-
-    drawChildren(){
-
+    draw(x, y, ctx){
+        this.screenX = x + this.xOffset;
+        this.screenY = y + this.yOffset;
+        ctx.drawImage(this.image, screenX, screenY);
+        this.children.forEach(child=>{child.draw(screenX, screenY, ctx)});
     }
 
     attachToParent(parent){
         this.parent = parent;
         parent.children.push(this);
+    }
+
+    initializeEvents(){
+        this.events = new EventEmitter();
+        canvas.addEventListener("mousedown", event=>{
+            var mx = event.clientX - canvas.offsetLeft;
+            var my = event.clientY - canvas.offsetTop;
+            if(this.mouseIsOver(mx, my)){
+                this.events.trigger("clicked", null);
+            }
+        });
+        this.gameCanvas.addEventListener('mousemove', event => {
+            var mx = event.clientX - canvas.offsetLeft;
+            var my = event.clientY - canvas.offsetTop;
+            if(this.mouseIsOver(mx, my)){
+                this.events.trigger("mouseover", null);
+            }
+        });
+    }
+
+    mouseIsOver(mx, my){
+        return this.screenX <= mx && mx <= this.screenX + this.w && this.screenY <= my && my <= this.screenY + this.h
     }
 
 }
