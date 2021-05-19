07e08ee8d407d769f778e134a5833e6501a11114
alexan32 <sethalexnader32@gmail.com>
2021-05-19 09:46:00 -0400

    interface event handling

--- engine/src/input.js
+++ engine/src/input.js
@@ -17,21 +17,23 @@ export class Input{
         this.mouse = {
             x: 0,
             y: 0,
-            down: false
+            down: false,
+            held: false
         };
         this.events = new EventEmitter();
         this.setEventBehavior();
+        this.heldTimeout = null;
     }
 
     // overwrite to set create custom input behavior.
     setEventBehavior(){
-        window.addEventListener("keydown", event =>{
+        this.gameCanvas.addEventListener("keydown", event =>{
             this.pressed[event.code] = true;
             if(["KeyW", "KeyA", "KeyS", "KeyD", "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].indexOf(event.code) != -1){
                 this.directional[event.code] = true;
             }
         });
-        window.addEventListener("keyup", event =>{
+        this.gameCanvas.addEventListener("keyup", event =>{
             delete this.pressed[event.code];
             if(Object.keys(this.pressed).length === 0){
                 this.events.trigger("noKeyInput");
@@ -45,21 +47,87 @@ export class Input{
         });
 
         this.gameCanvas.addEventListener('mousemove', event => this.onMousemove(event));
-        window.addEventListener('mouseup', event => this.onMouseup(event));
-        window.addEventListener('mousedown', event => this.onMousedown(event));
+        this.gameCanvas.addEventListener('mouseup', event => this.onMouseup());
+        this.gameCanvas.addEventListener('mousedown', event => this.onMousedown());
     }
 
     onMousemove(event){
-        this.mouse.x = event.clientX - this.gameCanvas.offsetLeft;
-        this.mouse.y = event.clientY - this.gameCanvas.offsetTop;
+        var x = event.clientX - this.gameCanvas.offsetLeft;
+        var y = event.clientY - this.gameCanvas.offsetTop;
+        this.mouse.x = x;
+        this.mouse.y = y;
+        this.events.trigger("mousemove", this.mouse);
     }
 
-    onMousedown(event){
+    onMousedown(){
         this.mouse.down = true;
+        this.events.trigger("mousedown", this.mouse);
+        this.heldTimeout = setTimeout(()=>{this.onMouseHeld(this.mouse)}, 500);
     }
 
-    onMouseup(event){
+    onMouseup(){
         this.mouse.down = false;
+        this.mouse.held = false;
+        this.events.trigger("mouseup", this.mouse);
+        clearTimeout(this.heldTimeout);
+    }
+
+    onMouseHeld(mouse){
+        mouse.held = true;
+        this.events.trigger("mouseheld", this.mouse);
+    }
+
+}
+
+
+/*  Use this object to filter out mouse events for a 
+    subregion of the canvas. mouse detector events will only
+    trigger when a mouse event occurs within a subregion of
+    the canvas.
+*/
+export class MouseDetector{
+
+    constructor(x, y, w, h, input){
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.events = new EventEmitter();
+        this.input = input;
+        input.events.on("mousedown", this.onMouseDown);
+        input.events.on("mouseup", this.onMouseUp);
+        input.events.on("mousemove", this.onMouseMove);
+        input.events.on("mouseheld", this.onMouseHeld);
+        // this.inBounds = (event)=>{
+        //     return this.x <= event.x && this.y <= event.y && this.x + this.w >=event.x && this.y + this.h >= event.y;
+        // }
+    }
+
+    onMouseDown(event){
+        if(this.x <= event.x && this.y <= event.y && this.x + this.w >=event.x && this.y + this.h >= event.y)
+            this.events.trigger("mousedown", event);
+    }
+
+    onMouseUp(event){
+        if(this.x <= event.x && this.y <= event.y && this.x + this.w >=event.x && this.y + this.h >= event.y)
+            this.events.trigger("mouseup", event);
+    }
+
+    onMouseMove(event){
+        if(this.x <= event.x && this.y <= event.y && this.x + this.w >=event.x && this.y + this.h >= event.y)
+            this.events.trigger("mousedown", event);
+    }
+
+    onMouseHeld(event){
+        if(this.x <= event.x && this.y <= event.y && this.x + this.w >=event.x && this.y + this.h >= event.y)
+            this.events.trigger("mouseheld", event);
+    }
+
+    destroy(){
+        this.input.events.removeCallback("mousedown", this.onMouseDown);
+        this.input.events.removeCallback("mouseup", this.onMouseUp);
+        this.input.events.removeCallback("mousemove", this.onMouseMove);
+        this.input.events.removeCallback("mouseheld", this.onMouseHeld);
     }
 
 }
--- project/arena/scripts/interface.js
+++ project/arena/scripts/interface.js
@@ -1,5 +1,6 @@
 import { EventEmitter } from "../../../engine/src/utils.js";
-import { viewFrame, canvas} from "../../../engine/main.js";
+import { viewFrame, canvas, input} from "../../../engine/main.js";
+import { MouseDetector } from "../../../engine/src/input.js";
 
 
 export class InterfaceTile{
@@ -13,9 +14,17 @@ export class InterfaceTile{
         this.screenY = 0;
         this.id = id;
 
+        // this.mouseDetector = new MouseDetector(0, 0, w, h, input);
+        this.events = new EventEmitter();
+        input.events.on("mousedown", this.onMouseDown);
+        input.events.on("mouseup", this.onMouseUp);
+        input.events.on("mousemove", this.onMouseMove);
+        input.events.on("mouseheld", this.onMouseHeld);
+
         this.image = image;
         this.parent = null;
         this.children = [];
+        this.initialDraw = true;
     }
 
 
@@ -26,13 +35,39 @@ export class InterfaceTile{
         this.screenX = x + this.xOffset;
         this.screenY = y + this.yOffset;
         ctx.drawImage(this.image, this.screenX, this.screenY, this.w, this.h);
-
         this.children.forEach(child=>{child.draw(ctx, this.screenX, this.screenY)});
     }
 
     setParent(parent){
         this.parent = parent;
         parent.children.push(this);
+    }
+
+    onMouseDown(event){
+        if(this.screenX <= event.x && this.screenY <= event.y && this.screenX + this.w >=event.x && this.screenY + this.h >= event.y)
+            this.events.trigger("mousedown", event);
+    }
+
+    onMouseUp(event){
+        if(this.screenX <= event.x && this.screenY <= event.y && this.screenX + this.w >=event.x && this.screenY + this.h >= event.y)
+            this.events.trigger("mouseup", event);
+    }
+
+    onMouseMove(event){
+        if(this.screenX <= event.x && this.screenY <= event.y && this.screenX + this.w >=event.x && this.screenY + this.h >= event.y)
+            this.events.trigger("mousedown", event);
+    }
+
+    onMouseHeld(event){
+        if(this.screenX <= event.x && this.screenY <= event.y && this.screenX + this.w >=event.x && this.screenY + this.h >= event.y)
+            this.events.trigger("mouseheld", event);
+    }
+
+    destroy(){
+        input.events.removeCallback("mousedown", this.onMouseDown);
+        input.events.removeCallback("mouseup", this.onMouseUp);
+        input.events.removeCallback("mousemove", this.onMouseMove);
+        input.events.removeCallback("mouseheld", this.onMouseHeld);
     }
 
 }
